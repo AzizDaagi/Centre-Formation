9c360d0279e53bbd1dd2149c5d1a612583792b4e
AzizDaagi <daagi.aziz09403@gmail.com>
2026-08-26 14:35:19 +0100

    Salle class: CRUD + Oracle round-trip verified

---
+++ salle.cpp
@@ -0,0 +1,101 @@
+#include "salle.h"
+#include "db.h"
+#include <QSqlQuery>
+#include <QSqlError>
+#include <QDebug>
+
+bool Salle::ajouter() {
+    QSqlQuery query(DB::instance().database());
+    query.prepare(
+        "INSERT INTO SALLE (NOM_SALLE, CAPACITE, TYPE_SALLE, STATUT) "
+        "VALUES (:nom, :capacite, :type, :statut)"
+        );
+    query.bindValue(":nom", m_nom);
+    query.bindValue(":capacite", m_capacite);
+    query.bindValue(":type", m_type);
+    query.bindValue(":statut", m_statut);
+
+    if (!query.exec()) {
+        qDebug() << "Erreur ajout salle:" << query.lastError().text();
+        return false;
+    }
+
+    // Récupère l'ID auto-généré pour cette nouvelle salle
+    QSqlQuery idQuery(DB::instance().database());
+    idQuery.prepare("SELECT MAX(ID_SALLE) FROM SALLE");
+    if (idQuery.exec() && idQuery.next()) {
+        m_id = idQuery.value(0).toInt();
+    }
+
+    return true;
+}
+
+bool Salle::modifier() {
+    QSqlQuery query(DB::instance().database());
+    query.prepare(
+        "UPDATE SALLE SET NOM_SALLE = :nom, CAPACITE = :capacite, "
+        "TYPE_SALLE = :type, STATUT = :statut WHERE ID_SALLE = :id"
+        );
+    query.bindValue(":nom", m_nom);
+    query.bindValue(":capacite", m_capacite);
+    query.bindValue(":type", m_type);
+    query.bindValue(":statut", m_statut);
+    query.bindValue(":id", m_id);
+
+    if (!query.exec()) {
+        qDebug() << "Erreur modification salle:" << query.lastError().text();
+        return false;
+    }
+    return true;
+}
+
+bool Salle::supprimer() {
+    QSqlQuery query(DB::instance().database());
+    query.prepare("DELETE FROM SALLE WHERE ID_SALLE = :id");
+    query.bindValue(":id", m_id);
+
+    if (!query.exec()) {
+        qDebug() << "Erreur suppression salle:" << query.lastError().text();
+        return false;
+    }
+    return true;
+}
+
+QList<Salle> Salle::listerTout() {
+    QList<Salle> resultats;
+    QSqlQuery query(DB::instance().database());
+    query.prepare("SELECT ID_SALLE, NOM_SALLE, CAPACITE, TYPE_SALLE, STATUT FROM SALLE ORDER BY ID_SALLE");
+
+    if (!query.exec()) {
+        qDebug() << "Erreur liste salles:" << query.lastError().text();
+        return resultats;
+    }
+
+    while (query.next()) {
+        resultats.append(Salle(
+            query.value(0).toInt(),
+            query.value(1).toString(),
+            query.value(2).toInt(),
+            query.value(3).toString(),
+            query.value(4).toString()
+            ));
+    }
+    return resultats;
+}
+
+Salle Salle::trouverParId(int id) {
+    QSqlQuery query(DB::instance().database());
+    query.prepare("SELECT ID_SALLE, NOM_SALLE, CAPACITE, TYPE_SALLE, STATUT FROM SALLE WHERE ID_SALLE = :id");
+    query.bindValue(":id", id);
+
+    if (query.exec() && query.next()) {
+        return Salle(
+            query.value(0).toInt(),
+            query.value(1).toString(),
+            query.value(2).toInt(),
+            query.value(3).toString(),
+            query.value(4).toString()
+            );
+    }
+    return Salle(); // salle vide/invalide si non trouvée
+}
---
+++ salle.h
@@ -0,0 +1,42 @@
+#ifndef SALLE_H
+#define SALLE_H
+
+#include <QString>
+#include <QList>
+
+class Salle {
+public:
+    Salle() = default;
+    Salle(int id, const QString& nom, int capacite, const QString& type, const QString& statut)
+        : m_id(id), m_nom(nom), m_capacite(capacite), m_type(type), m_statut(statut) {}
+
+    // Getters
+    int id() const { return m_id; }
+    QString nom() const { return m_nom; }
+    int capacite() const { return m_capacite; }
+    QString type() const { return m_type; }
+    QString statut() const { return m_statut; }
+
+    // Setters
+    void setId(int id) { m_id = id; }
+    void setNom(const QString& nom) { m_nom = nom; }
+    void setCapacite(int capacite) { m_capacite = capacite; }
+    void setType(const QString& type) { m_type = type; }
+    void setStatut(const QString& statut) { m_statut = statut; }
+
+    // CRUD - opérations sur la base de données
+    bool ajouter();
+    bool modifier();
+    bool supprimer();
+    static QList<Salle> listerTout();
+    static Salle trouverParId(int id);
+
+private:
+    int m_id = -1;
+    QString m_nom;
+    int m_capacite = 0;
+    QString m_type;
+    QString m_statut = "DISPONIBLE";
+};
+
+#endif // SALLE_H
